c4c20cf856110452975a5f1b7b7e91c1c4557205
KeeGooRoomiE <59682188+KeeGooRoomiE@users.noreply.github.com>
2021-02-01 16:59:03 +0700

    Update button timer

--- Assets/ButtonCountdown.cs
+++ Assets/ButtonCountdown.cs
@@ -7,26 +7,42 @@ public class ButtonCountdown : MonoBehaviour
 {
     public Button Button;
     public float TimeToAwake;
+    public float curTime;
     public float period = 3.0f;
     public Text textComp;
+    public bool isTimeSet;
 
     private void Start() {
     //check triggers
     Button.interactable = false;
-    TimeToAwake = Time.time + 3.0f;
+    //TimeToAwake = Time.time + period;
+    isTimeSet = false;
     }
 
     void Update() {
-       
+       curTime = Time.time;
+
+        if (isTimeSet == false) {
+            isTimeSet = true;
+            TimeToAwake = curTime + 3.0f;
+        }
+
+
         if (Time.time > TimeToAwake ) {
             Button.interactable = true;
-            TimeToAwake += period;
+            //TimeToAwake += period;
         }
 
         if (Button.interactable == true) {
             textComp.text = "";
+            
         } else {
             textComp.text = (-1*((int)Time.time - (int)TimeToAwake)).ToString();
         }
+    }
+
+    public void ResetTimeSet() {
+        isTimeSet = false;
+        Button.interactable = false;
     }
 }
